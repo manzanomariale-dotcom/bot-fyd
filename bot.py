0030ad417c1b9112719f43ea1779897aa831561b
manzanomariale-dotcom <manzanomariale@gmail.com>
2026-08-04 17:00:07 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -66,11 +66,12 @@ TRADUCCION_LOTERIAS = {
 HEADER_FyD = (
     "Resultado: *AGENCIA FyD*\n"
     "Hora: {hora_str}\n"
-    "JUEGA AQUI\n"
+    
     "RESULTADOS ANIMALITOS\n\n"
     "🎲 *{nombre_loteria}* 🎲\n"
     "Hora: {hora}\n"
     "Animalito: *{resultado}*\n\n"
+    "JUEGA AQUI\n"
     f"{ENLACE_CANAL}"
 )
 
