8147ab1c82c56269effb266476b9b2a8548be4ef
manzanomariale-dotcom <manzanomariale@gmail.com>
2026-08-04 09:37:34 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -41,14 +41,11 @@ URL_BCV = 'https://www.bcv.org.ve/'
 ARCH_REGISTRO = "resultados_enviados.json"
 
 HEADER_FyD = (
-    "RESULTADO OFICIAL\n"
+    "Resultado: *AGENCIA FyD*\n"
     "Hora: {hora_str}\n"
-    "Resultado: AGENCIA FyD\n"
-    "_Trabajamos para tí_\n"
     "JUEGA AQUI\n"
-    "WHATSAPP: 04249611372\n"
     "RESULTADOS ANIMALITOS\n\n"
-    "🎲 {nombre_loteria} 🎲\n"
+    "🎲 *{nombre_loteria}* 🎲\n"
     "Hora: {hora}\n"
     "Animalito: *{resultado}*\n\n"
     f"{ENLACE_CANAL}"
